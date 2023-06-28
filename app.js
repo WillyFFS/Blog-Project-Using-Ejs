40810a2ff7b159c9385ee5a2cbd71e555d8f0c9b
WillyFFS <willyfrans13@gmail.com>
2023-06-28 21:22:16 +0700

    connect to database mongodb atlas

--- app.js
+++ app.js
@@ -5,20 +5,36 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent = "WELCOME TO MY SIMPLE BLOG PROJECT. To create a new post you can click compose.";
+const guide =  "To create a new post you can click compose. To see the whole text you can click read more";
+const aboutContent = "This blog is using ejs, node.js, express, and mongoDB.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express(); 
-
-let arrDiary =[];
+const mongoose = require("mongoose");
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+mongoose.connect("mongodb+srv://willyfarel131003:****@cluster0.ekkf39f.mongodb.net/blogDB");
+
+const postschema = {
+  title : String,
+  content: String
+}
+
+const Post = mongoose.model("post", postschema);
+
 app.get("/",function(req,res){
-  res.render("home",{textHome: homeStartingContent, arrDiary: arrDiary});
+  Post.find({})
+    .then((post)=>{    
+      res.render("home",{textHome: homeStartingContent,guide: guide, posts: post });
+    });
 });
 
 app.get("/about",function(req,res){
@@ -34,26 +50,24 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  const diary = {
+  const post = new Post({
     title: req.body.textTitle,
-    body: req.body.textPost
-  }
-  arrDiary.push(diary);
-  res.redirect("/");
-});
-
-app.get("/posts/:diaryTitle",function(req,res){
-  arrDiary.forEach(function(diary){
-    var param = req.params.diaryTitle;
-    if(_.lowerCase(diary.title ) === _.lowerCase(param)){
-      res.render("post",{diaryTtile: diary.title, diaryBody: diary.body})
-    }
-    else{
-      console.log("Not Found!");
-    }
+    content: req.body.textPost
   });
+  post.save();
+  Post.findById(post._id)
+    .then((found)=>{
+      if (found){
+      res.redirect("/");}
+    })
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.get("/posts/:blogId",function(req,res){
+  var param = req.params.blogId;
+  Post.findById(param)
+    .then((found)=>{
+      res.render("post",{blogTitle: found.title, blogContent: found.content});
+    })
 });
+
+
